d5c5bde983f24c0b3c43b06ead94c77f8d4e036e
Eddie Kiernan <edkiernaniii@gmail.com>
2023-12-01 10:22:36 -0700

    tables, added. Need to test some functionality before merge

--- All Project Code/src/init_data/insert.sql
+++ All Project Code/src/init_data/insert.sql
@@ -2,3 +2,13 @@ INSERT INTO users (username, password, email) VALUES
 ('ek3', 'password123', 'ekthree@gmail.com'),
 ('coinflip', 'complexpw', 'frankthetank@yahoo.com'),
 ('profeggman','collectrings','eggman@colorado.edu');
+
+INSERT INTO pdf (name) VALUES
+('test.pdf'),
+('discrete.pdf'),
+('book.pdf');
+
+INSERT INTO users_to_pdf (email, pdf_id) VALUES
+('ekthree@gmail.com', 1),
+('eggman@colorado.edu', 2),
+('ekthree@gmail.com', 3);
