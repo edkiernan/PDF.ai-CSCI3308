adcc7f598929f59ffcaa1df07caf7f8ff48f8869
Eddie Kiernan <edkiernaniii@gmail.com>
2023-12-01 14:12:03 -0700

    slight change to insert tables

--- All Project Code/src/init_data/create.sql
+++ All Project Code/src/init_data/create.sql
@@ -6,30 +6,29 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 DROP TABLE IF EXISTS pdf CASCADE;
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS pdf (
   pdf_id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(100) NOT NULL
 );
 
 DROP TABLE IF EXISTS users_to_pdf CASCADE;
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users_to_pdf (
   email VARCHAR(60) NOT NULL,
   pdf_id INT NOT NULL,
   FOREIGN KEY (email) REFERENCES users (email) ON DELETE CASCADE,
   FOREIGN KEY (pdf_id) REFERENCES pdf (pdf_id) ON DELETE CASCADE
 );
 
-INSERT INTO users (username, password, email) VALUES
-('ek3', 'password123', 'ekthree@gmail.com'),
-('coinflip', 'complexpw', 'frankthetank@yahoo.com'),
-('profeggman','collectrings','eggman@colorado.edu');
+INSERT INTO users (username, password, email) VALUES ('ek3', 'password123', 'ekthree@gmail.com');
+INSERT INTO users (username, password, email) VALUES ('coinflip', 'complexpw', 'frankthetank@yahoo.com');
+INSERT INTO users (username, password, email) VALUES ('profeggman','collectrings','eggman@colorado.edu');
 
-INSERT INTO pdf (name) VALUES
-('test.pdf'),
-('discrete.pdf'),
-('book.pdf');
+INSERT INTO pdf (name) VALUES ('test.pdf');
+INSERT INTO pdf (name) VALUES ('discrete.pdf');
+INSERT INTO pdf (name) VALUES ('book.pdf');
 
-INSERT INTO users_to_pdf (email, pdf_id) VALUES
-('ekthree@gmail.com', 1),
-('eggman@colorado.edu', 2),
-('ekthree@gmail.com', 3);
+INSERT INTO users_to_pdf (email, pdf_id) VALUES ('ekthree@gmail.com', 1);
+INSERT INTO users_to_pdf (email, pdf_id) VALUES ('eggman@colorado.edu', 2);
+INSERT INTO users_to_pdf (email, pdf_id) VALUES ('ekthree@gmail.com', 3);
+
+\dt;
