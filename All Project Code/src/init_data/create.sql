711569555859e944ef304a1f70fd0af94dd13940
Eddie Kiernan <edkiernaniii@gmail.com>
2023-12-01 13:42:28 -0700

    should be complete and working database. Condolidated create.sql and insert.sql. The test INSERTS should now work for testing purposes.

--- All Project Code/src/init_data/create.sql
+++ All Project Code/src/init_data/create.sql
@@ -18,3 +18,18 @@ CREATE TABLE IF NOT EXISTS users (
   FOREIGN KEY (email) REFERENCES users (email) ON DELETE CASCADE,
   FOREIGN KEY (pdf_id) REFERENCES pdf (pdf_id) ON DELETE CASCADE
 );
+
+INSERT INTO users (username, password, email) VALUES
+('ek3', 'password123', 'ekthree@gmail.com'),
+('coinflip', 'complexpw', 'frankthetank@yahoo.com'),
+('profeggman','collectrings','eggman@colorado.edu');
+
+INSERT INTO pdf (name) VALUES
+('test.pdf'),
+('discrete.pdf'),
+('book.pdf');
+
+INSERT INTO users_to_pdf (email, pdf_id) VALUES
+('ekthree@gmail.com', 1),
+('eggman@colorado.edu', 2),
+('ekthree@gmail.com', 3);
